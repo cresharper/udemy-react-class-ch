045078f6aa029480c04c0b88336197956cb5055b
Connor Harper <connor.harper@floqast.com>
2021-11-11 18:24:15 -0500

    two way binding, event submission etc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -23,7 +24,10 @@ const App = () => {
     },
   ];
   return (
-    <Expenses items={expenses} />
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
