e3a6163fe34f8a1221ac8a24b20fadb535c6f6d8
Connor Harper <connor.harper@floqast.com>
2022-01-19 11:12:49 -0500

    idk

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,30 +14,22 @@ function Expenses(props) {
     setPickedYear(filteredYear);
   }
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return(
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={pickedYear} onFilterYear={selectedFilterYear} />
-        <ExpenseItem 
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date} 
-        />
-        <ExpenseItem 
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date} 
-        />
-        <ExpenseItem 
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date} 
-        />
-        <ExpenseItem 
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date} 
-        />
+        {filteredExpenses.map(expense => 
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} 
+          />
+        )}
       </Card>
     </div>
   )
